77e1fde57d1821a647ae4e3aaf2a4a3398077e0d
Octavio44635 <81695953+Octavio44635@users.noreply.github.com>
2025-09-07 16:29:37 -0300

    .

--- Sugeno/Practica/sugeno3.py
+++ Sugeno/Practica/sugeno3.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import SugenoCampus as SC
 from sklearn.metrics import mean_squared_error
 
+#Conviene usar kmeans
 
 def funcionExp(x,A,B,C):
     return A*np.exp(-B*x)+C
@@ -11,15 +12,15 @@ data_x = np.arange(-5,10, 0.2)
 data_y = funcionExp(data_x,2,0.5,5)
 noisy_y = data_y + np.random.randn(len(data_x))
 
-data = np.vstack((data_x, data_y)).T
+data = np.vstack((data_x, noisy_y)).T
 
 fis2 = SC.fis() #crea el objeto fis
-fis2.genfis(data, 1.1) #genera datos en fis
+fis2.genfis(data, 1.15) #genera datos en fis
 fis2.viewInputs()
 r = fis2.evalfis(np.vstack(data_x))
 
 y_pred = fis2.evalfis(np.vstack(data_x))
-mse = mean_squared_error(data_y, y_pred)
+mse = mean_squared_error(noisy_y, y_pred)
 print(f"Error cuadrático medio: {mse:.12f}")
 plt.show()
 
